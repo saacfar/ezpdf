8268909e8ced172ec5855f4865b1d64be1da9bf2
saacfar <saacfar@yahoo.com>
2024-08-14 21:21:12 +0000

    replace each() with array_key_first() for php8 replace each() with array_key_first() for php8 compatibility

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rebuy/ezpdf',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5f906c3b74e1d14db57d6cf4c09cc8918dd67390',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rebuy/ezpdf' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5f906c3b74e1d14db57d6cf4c09cc8918dd67390',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
